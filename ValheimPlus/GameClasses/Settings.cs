bde7a51828e2341b925bdbcf2407af4df44215f6
Lilian Cahuzac <4863386+healiha@users.noreply.github.com>
2023-12-05 00:31:07 +0100

    Fix mute background music setting

--- ValheimPlus/GameClasses/Settings.cs
+++ ValheimPlus/GameClasses/Settings.cs
@@ -9,43 +9,54 @@ namespace ValheimPlus.GameClasses
     {
         public static Toggle muteAudioToggle;
 
-        public static void CreateToggle()
+        public static bool CreateToggle()
         {
-            Toggle cmToggle = Settings.instance.m_continousMusic;
-            muteAudioToggle = GameObject.Instantiate(cmToggle, cmToggle.transform.parent, false);
-            muteAudioToggle.name = "MuteGameInBackground";
-            muteAudioToggle.GetComponentInChildren<TMP_Text>().text = "Mute Game in Background";
+            foreach (var iSettingsTab in Settings.instance.SettingsTabs)
+            {
+                if (iSettingsTab.GetType() == typeof(Fishlabs.Valheim.AudioSettings))
+                {
+                    Toggle cmToggle = ((Fishlabs.Valheim.AudioSettings)iSettingsTab).m_continousMusic;
+                    muteAudioToggle = GameObject.Instantiate(cmToggle, cmToggle.transform.parent, false);
+                    muteAudioToggle.name = "MuteGameInBackground";
+                    muteAudioToggle.GetComponentInChildren<TMP_Text>().text = "Mute game in background";
 
-            // scaleFactor is overwritten by GuiScaler::UpdateScale, which is called every frame, but impacted when pressing OK in the settings dialog
-            CanvasScaler canvasScalerComponent = muteAudioToggle.transform.root.GetComponentInChildren<CanvasScaler>();
-            muteAudioToggle.transform.Translate(new Vector2(0, -40 * canvasScalerComponent.scaleFactor));
+                    // scaleFactor is overwritten by GuiScaler::UpdateScale, which is called every frame, but impacted when pressing OK in the settings dialog
+                    CanvasScaler canvasScalerComponent = muteAudioToggle.transform.root.GetComponentInChildren<CanvasScaler>();
+                    muteAudioToggle.transform.Translate(new Vector2(0, -40 * canvasScalerComponent.scaleFactor));
+                    return true;
+                }
+            }
+
+            ValheimPlusPlugin.Logger.LogError("Failed to create MuteGameInBackground toggle");
+            return false;
         }
     }
 
     /// <summary>
     /// Read in saved user preference for audio mute toggle
     /// </summary>
-    [HarmonyPatch(typeof(Settings), "Awake")]
+    [HarmonyPatch(typeof(Settings), nameof(Settings.Awake))]
     public static class Settings_LoadSettings_Patch
     {
         private static void Postfix()
         {
-            if (MuteGameInBackground.muteAudioToggle == null)
-                MuteGameInBackground.CreateToggle();
+           if (MuteGameInBackground.muteAudioToggle == null && !MuteGameInBackground.CreateToggle())
+                return;
 
-            MuteGameInBackground.muteAudioToggle.isOn = (PlayerPrefs.GetInt("MuteGameInBackground", 0) == 1); ;
+           MuteGameInBackground.muteAudioToggle.isOn = (PlayerPrefs.GetInt("MuteGameInBackground", 0) == 1); ;
         }
     }
 
     /// <summary>
     /// Save out user preference for audio mute toggle
     /// </summary>
-    [HarmonyPatch(typeof(Settings), "SaveSettings")]
+    [HarmonyPatch(typeof(Settings), nameof(Settings.SaveTabSettings))]
     public static class Settings_SaveSettings_Patch
     {
         private static void Postfix()
         {
-            PlayerPrefs.SetInt("MuteGameInBackground", MuteGameInBackground.muteAudioToggle.isOn ? 1 : 0);
+            if (MuteGameInBackground.muteAudioToggle != null)
+                PlayerPrefs.SetInt("MuteGameInBackground", MuteGameInBackground.muteAudioToggle.isOn ? 1 : 0);
         }
     }
 }
