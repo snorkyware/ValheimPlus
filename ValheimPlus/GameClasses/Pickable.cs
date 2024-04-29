8282c1a567dfc9b22464cfd17f4cba93e7de5831
Grantapher <granttpfr@gmail.com>
2024-04-28 19:29:38 -0700

    #23 Fix `Pickable` not applying to certain items
    Some items use `PickableItem` instead of `Pickable`, so another patch was necessary to cover the items that weren't working.

--- ValheimPlus/GameClasses/Pickable.cs
+++ ValheimPlus/GameClasses/Pickable.cs
@@ -1,8 +1,9 @@
-﻿using HarmonyLib;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
+using HarmonyLib;
+using JetBrains.Annotations;
 using UnityEngine;
 using ValheimPlus.Configurations;
 
@@ -10,68 +11,19 @@ namespace ValheimPlus.GameClasses
 {
     public static class PickableYieldState
     {
-        public static Dictionary<string, float> yieldModifier;
-    }
+        private static Dictionary<string, float> _yieldModifierDict;
 
-    /// <summary>
-    /// Allow tweaking of Pickable item yield. (E.g. berries, flowers, branches, stones, gemstones.)
-    /// </summary>
-    [HarmonyPatch(typeof(Pickable), "RPC_Pick")]
-    public static class Pickable_RPC_Pick_Transpiler
-    {
-        // Our method and its arguments that we need to patch in
-        private static readonly MethodInfo method_calculateYield = AccessTools.Method(typeof(Pickable_RPC_Pick_Transpiler), nameof(calculateYield));
-        private static readonly FieldInfo field_ItemPrefab = AccessTools.Field(typeof(Pickable), "m_itemPrefab");
-        private static readonly FieldInfo field_amount = AccessTools.Field(typeof(Pickable), "m_amount");
-
-        [HarmonyTranspiler]
-        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        public static int CalculateYield(GameObject item, int originalAmount)
         {
-            if (!Configuration.Current.Pickable.IsEnabled)
-                return instructions;
-
-            List<CodeInstruction> il = instructions.ToList();
-            for (int i = 0; i < il.Count; i++)
+            if (_yieldModifierDict.TryGetValue(item.name, out float yieldModifier))
             {
-                if (il[i].opcode == OpCodes.Stloc_1)
-                {
-                    // Call calculateYield() and replace the original drop amount with the result.
-                    // Calling calculateYield takes several instructions:
-                    // LdArg.0 (load the "this" pointer), LdFld (load m_itemPrefab from this),
-                    // Ldloc.1 (load the amount the game originally wants to drop from local 1), Call.
-                    // We then Stloc.1 to store the return value back into local 1 so that the game uses our
-                    // modified drop amount rather than the original.
-                    il.Insert(++i, new CodeInstruction(OpCodes.Ldarg_0));
-                    il.Insert(++i, new CodeInstruction(OpCodes.Ldfld, field_ItemPrefab));
-                    il.Insert(++i, new CodeInstruction(OpCodes.Ldloc_1));
-                    il.Insert(++i, new CodeInstruction(new CodeInstruction(OpCodes.Call, method_calculateYield)));
-                    il.Insert(++i, new CodeInstruction(OpCodes.Stloc_1));
-
-                    // NOTE: This transpiler may be called multiple times, e.g. when starting the game, when connecting to a server and when disconnecting.
-                    // We need to re-do the initial setup every time, since the modifier values may have changed (as the server config will be used instead of the client config).
-                    initialSetup();
-
-                    return il.AsEnumerable();
-                }
+                return (int)Helper.applyModifierValue(originalAmount, yieldModifier);
             }
 
-            ValheimPlusPlugin.Logger.LogError("Unable to transpile Pickable.RPC_Pick to patch item yields");
-            return instructions;
+            return originalAmount;
         }
 
-        private static int calculateYield(GameObject item, int originalAmount)
-        {
-            try
-            {
-                return (int)Helper.applyModifierValue(originalAmount, PickableYieldState.yieldModifier[item.name]);
-            }
-            catch
-            {
-                return originalAmount;
-            }
-        }
-
-        private static void initialSetup()
+        public static void InitialSetup()
         {
             // Called from the transpiler, so this will be run when the game starts, plus when you connect to or disconnect from a server.
 
@@ -125,20 +77,72 @@ namespace ValheimPlus.GameClasses
                 "BlackCore"
             };
 
-            PickableYieldState.yieldModifier = new Dictionary<string, float>();
+            _yieldModifierDict = new Dictionary<string, float>();
 
             foreach (var item in edibles)
-                PickableYieldState.yieldModifier.Add(item, Configuration.Current.Pickable.edibles);
+                _yieldModifierDict.Add(item, Configuration.Current.Pickable.edibles);
             foreach (var item in flowersAndIngredients)
-                PickableYieldState.yieldModifier.Add(item, Configuration.Current.Pickable.flowersAndIngredients);
+                _yieldModifierDict.Add(item, Configuration.Current.Pickable.flowersAndIngredients);
             foreach (var item in materials)
-                PickableYieldState.yieldModifier.Add(item, Configuration.Current.Pickable.materials);
+                _yieldModifierDict.Add(item, Configuration.Current.Pickable.materials);
             foreach (var item in valuables)
-                PickableYieldState.yieldModifier.Add(item, Configuration.Current.Pickable.valuables);
+                _yieldModifierDict.Add(item, Configuration.Current.Pickable.valuables);
             foreach (var item in surtlingCores)
-                PickableYieldState.yieldModifier.Add(item, Configuration.Current.Pickable.surtlingCores);
+                _yieldModifierDict.Add(item, Configuration.Current.Pickable.surtlingCores);
             foreach (var item in blackCores)
-                PickableYieldState.yieldModifier.Add(item, Configuration.Current.Pickable.blackCores);
+                _yieldModifierDict.Add(item, Configuration.Current.Pickable.blackCores);
+        }
+    }
+
+    /// <summary>
+    /// Allow tweaking of Pickable item yield. (E.g. berries, flowers, branches, stones, gemstones.)
+    /// </summary>
+    [HarmonyPatch(typeof(Pickable), nameof(Pickable.RPC_Pick))]
+    public static class Pickable_RPC_Pick_Transpiler
+    {
+        // Our method and its arguments that we need to patch in
+        private static readonly MethodInfo Method_CalculateYield =
+            AccessTools.Method(typeof(PickableYieldState), nameof(PickableYieldState.CalculateYield));
+
+        private static readonly FieldInfo Field_ItemPrefab = AccessTools.Field(typeof(Pickable), "m_itemPrefab");
+
+        [HarmonyTranspiler]
+        [UsedImplicitly]
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions,
+            ILGenerator generator)
+        {
+            if (!Configuration.Current.Pickable.IsEnabled)
+                return instructions;
+
+            List<CodeInstruction> il = instructions.ToList();
+            for (int i = 0; i < il.Count; i++)
+            {
+                if (il[i].opcode == OpCodes.Stloc_1)
+                {
+                    // Call calculateYield() and replace the original drop amount with the result.
+                    // Calling calculateYield takes several instructions:
+                    // LdArg.0 (load the "this" pointer), LdFld (load m_itemPrefab from this),
+                    // Ldloc.1 (load the amount the game originally wants to drop from local 1), Call.
+                    // We then Stloc.1 to store the return value back into local 1 so that the game uses our
+                    // modified drop amount rather than the original.
+                    il.Insert(++i, new CodeInstruction(OpCodes.Ldarg_0));
+                    il.Insert(++i, new CodeInstruction(OpCodes.Ldfld, Field_ItemPrefab));
+                    il.Insert(++i, new CodeInstruction(OpCodes.Ldloc_1));
+                    il.Insert(++i, new CodeInstruction(new CodeInstruction(OpCodes.Call, Method_CalculateYield)));
+                    il.Insert(++i, new CodeInstruction(OpCodes.Stloc_1));
+
+                    // NOTE: This transpiler may be called multiple times, e.g. when starting the game,
+                    // when connecting to a server and when disconnecting. We need to re-do the initial
+                    // setup every time, since the modifier values may have changed (as the server config
+                    // will be used instead of the client config).
+                    PickableYieldState.InitialSetup();
+
+                    return il.AsEnumerable();
+                }
+            }
+
+            ValheimPlusPlugin.Logger.LogError("Unable to transpile Pickable.RPC_Pick to patch item yields");
+            return il;
         }
     }
 }
---
+++ ValheimPlus/GameClasses/PickableItem.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using HarmonyLib;
+using JetBrains.Annotations;
+using UnityEngine;
+using ValheimPlus.Configurations;
+using Object = UnityEngine.Object;
+using Random = UnityEngine.Random;
+
+namespace ValheimPlus.GameClasses
+{
+    [HarmonyPatch(typeof(PickableItem), nameof(PickableItem.GetStackSize))]
+    public static class PickableItem_GetStackSize_Patch
+    {
+        [UsedImplicitly]
+        public static void Postfix(PickableItem __instance, ref int __result)
+        {
+            __result = PickableYieldState.CalculateYield(__instance.m_itemPrefab.gameObject, __result);
+        }
+    }
+
+    [HarmonyPatch(typeof(PickableItem), nameof(PickableItem.Drop))]
+    public static class PickableItem_Drop_Prefix
+    {
+        [UsedImplicitly]
+        public static bool Prefix(PickableItem __instance)
+        {
+            if (!Configuration.Current.Pickable.IsEnabled)
+                return true;
+
+            // The original code only drops one item at most, so we have to overwrite it to drop multiple.
+            var maxStackSize = __instance.m_itemPrefab.m_itemData.m_shared.m_maxStackSize;
+            var stackSize = __instance.GetStackSize();
+
+            var offset = 0;
+            while (stackSize > 0)
+            {
+                Drop(__instance, __instance.m_itemPrefab.gameObject, offset++, Math.Min(maxStackSize, stackSize));
+                stackSize -= maxStackSize;
+            }
+
+            return false;
+        }
+
+        // this is copy/pasted from `Pickable.Drop` with `component` replacing `this`
+        // and spawnOffset replaced by 0.2f like in the `PickableItem.Drop`
+        private static void Drop(Component component, GameObject prefab, int offset, int stack)
+        {
+            var vector2 = Random.insideUnitCircle * 0.2f;
+            var position = component.transform.position + Vector3.up * 0.2f +
+                           new Vector3(vector2.x, 0.2f * offset, vector2.y);
+            var rotation = Quaternion.Euler(0.0f, Random.Range(0, 360), 0.0f);
+            var gameObject = Object.Instantiate(prefab, position, rotation);
+            var itemDrop = gameObject.GetComponent<ItemDrop>();
+            if (itemDrop != null)
+            {
+                itemDrop.SetStack(stack);
+                ItemDrop.OnCreateNew(itemDrop);
+            }
+
+            gameObject.GetComponent<Rigidbody>().velocity = Vector3.up * 4f;
+        }
+    }
+}
