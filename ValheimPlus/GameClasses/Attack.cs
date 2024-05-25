2841d56c2b96885262f3c011b82019a3405d4780
Grantapher <granttpfr@gmail.com>
2024-05-24 18:22:56 -0700

    Clean up `Attack.cs` and the `valheim_plus.cfg` files

--- ValheimPlus/GameClasses/Attack.cs
+++ ValheimPlus/GameClasses/Attack.cs
@@ -1,87 +1,66 @@
 using HarmonyLib;
+using JetBrains.Annotations;
 using UnityEngine;
 using ValheimPlus.Configurations;
 
 namespace ValheimPlus.GameClasses
 {
+    public static class AttackExtensions
+    {
+        public static Skills.SkillType GetCharacterWeaponSkillType(this Attack __instance) =>
+            __instance.m_character.GetCurrentWeapon()?.m_shared?.m_skillType ?? Skills.SkillType.Unarmed;
+
+        public static bool IsAttackFromLocalPlayer(this Attack __instance) =>
+            __instance.m_character == Player.m_localPlayer;
+    }
+
     /// <summary>
     /// Alters stamina of weapons
     /// </summary>
     [HarmonyPatch(typeof(Attack), nameof(Attack.GetAttackStamina))]
     public static class Attack_GetAttackStamina_Patch
     {
+        [UsedImplicitly]
         private static void Postfix(ref Attack __instance, ref float __result)
         {
-            if (!Configuration.Current.StaminaUsage.IsEnabled || __instance.m_character != Player.m_localPlayer) return;
+            if (!Configuration.Current.StaminaUsage.IsEnabled || !__instance.IsAttackFromLocalPlayer()) return;
 
-            ItemDrop.ItemData item = __instance.m_character.GetCurrentWeapon();
-            Skills.SkillType skillType;
-            if (item == null)
+            var modifier = __instance.GetCharacterWeaponSkillType() switch
             {
-                skillType = Skills.SkillType.Unarmed;
-            }
-            else
-            {
-                skillType = item.m_shared.m_skillType;
-            }
+                Skills.SkillType.Swords => Configuration.Current.StaminaUsage.swords,
+                Skills.SkillType.Knives => Configuration.Current.StaminaUsage.knives,
+                Skills.SkillType.Clubs => Configuration.Current.StaminaUsage.clubs,
+                Skills.SkillType.Polearms => Configuration.Current.StaminaUsage.polearms,
+                Skills.SkillType.Spears => Configuration.Current.StaminaUsage.spears,
+                Skills.SkillType.Axes => Configuration.Current.StaminaUsage.axes,
+                Skills.SkillType.Unarmed => Configuration.Current.StaminaUsage.unarmed,
+                Skills.SkillType.Pickaxes => Configuration.Current.StaminaUsage.pickaxes,
+                Skills.SkillType.Bows => Configuration.Current.StaminaUsage.bows,
+                _ => 0f
+            };
 
-            switch (skillType)
-            {
-                case Skills.SkillType.Swords:
-                    __result = Helper.applyModifierValue(__result, Configuration.Current.StaminaUsage.swords);
-                    break;
-                case Skills.SkillType.Knives:
-                    __result = Helper.applyModifierValue(__result, Configuration.Current.StaminaUsage.knives);
-                    break;
-                case Skills.SkillType.Clubs:
-                    __result = Helper.applyModifierValue(__result, Configuration.Current.StaminaUsage.clubs);
-                    break;
-                case Skills.SkillType.Polearms:
-                    __result = Helper.applyModifierValue(__result, Configuration.Current.StaminaUsage.polearms);
-                    break;
-                case Skills.SkillType.Spears:
-                    __result = Helper.applyModifierValue(__result, Configuration.Current.StaminaUsage.spears);
-                    break;
-                case Skills.SkillType.Axes:
-                    __result = Helper.applyModifierValue(__result, Configuration.Current.StaminaUsage.axes);
-                    break;
-                case Skills.SkillType.Unarmed:
-                    __result = Helper.applyModifierValue(__result, Configuration.Current.StaminaUsage.unarmed);
-                    break;
-                case Skills.SkillType.Pickaxes:
-                    __result = Helper.applyModifierValue(__result, Configuration.Current.StaminaUsage.pickaxes);
-                    break;
-                case Skills.SkillType.Bows:
-                    __result = Helper.applyModifierValue(__result, Configuration.Current.StaminaUsage.bows);
-                    break;
-                default:
-                    break;
-            }
-            
+            if (modifier == 0f) return;
+            __result = Helper.applyModifierValue(__result, modifier);
         }
     }
 
     [HarmonyPatch(typeof(Attack), nameof(Attack.GetAttackEitr))]
     public static class Attack_GetAttackEitr_Patch
     {
+        [UsedImplicitly]
         private static void Postfix(ref Attack __instance, ref float __result)
         {
-            if (!Configuration.Current.EitrUsage.IsEnabled || __instance.m_character != Player.m_localPlayer) return;
+            if (!Configuration.Current.EitrUsage.IsEnabled || !__instance.IsAttackFromLocalPlayer()) return;
 
-            ItemDrop.ItemData item = __instance.m_character.GetCurrentWeapon();
-            if (item == null) return;
-
-            switch (item.m_shared.m_skillType)
+            var modifier = __instance.GetCharacterWeaponSkillType() switch
             {
-                case Skills.SkillType.BloodMagic:
-                    __result = Helper.applyModifierValue(__result, Configuration.Current.EitrUsage.bloodMagic);
-                    break;
-                case Skills.SkillType.ElementalMagic:
-                    __result = Helper.applyModifierValue(__result, Configuration.Current.EitrUsage.elementalMagic);
-                    break;
-                default:
-                    break;
-            }
+                Skills.SkillType.BloodMagic => Configuration.Current.EitrUsage.bloodMagic,
+                Skills.SkillType.ElementalMagic => Configuration.Current.EitrUsage.elementalMagic,
+                _ => 0f
+            };
+
+            if (modifier == 0f) return;
+            __result = Helper.applyModifierValue(__result, modifier);
         }
     }
 
@@ -89,21 +68,19 @@ namespace ValheimPlus.GameClasses
     [HarmonyPatch(typeof(Attack), nameof(Attack.GetAttackHealth))]
     public static class Attack_GetAttackHealth_Patch
     {
+        [UsedImplicitly]
         private static void Postfix(ref Attack __instance, ref float __result)
         {
-            if (!Configuration.Current.HealthUsage.IsEnabled || __instance.m_character != Player.m_localPlayer) return;
+            if (!Configuration.Current.HealthUsage.IsEnabled || !__instance.IsAttackFromLocalPlayer()) return;
 
-            ItemDrop.ItemData item = __instance.m_character.GetCurrentWeapon();
-            if (item == null) return;
-
-            switch (item.m_shared.m_skillType)
+            var modifier = __instance.GetCharacterWeaponSkillType() switch
             {
-                case Skills.SkillType.BloodMagic:
-                    __result = Helper.applyModifierValue(__result, Configuration.Current.HealthUsage.bloodMagic);
-                    break;
-                default:
-                    break;
-            }
+                Skills.SkillType.BloodMagic => Configuration.Current.HealthUsage.bloodMagic,
+                _ => 0f
+            };
+
+            if (modifier == 0f) return;
+            __result = Helper.applyModifierValue(__result, modifier);
         }
     }
 
@@ -113,70 +90,66 @@ namespace ValheimPlus.GameClasses
     [HarmonyPatch(typeof(Attack), nameof(Attack.ProjectileAttackTriggered))]
     public static class Attack_ProjectileAttackTriggered_Patch
     {
-        private static float maxClampValue = 1E+6f;
-
+        [UsedImplicitly]
         private static void Prefix(ref Attack __instance)
         {
-            if (Configuration.Current.PlayerProjectile.IsEnabled && __instance.m_character.IsPlayer())
-            {
-                // This might not be required but i wanted to add this non the less to be sure that no missing properties are causing issues.
-                if (__instance?.m_projectileVel == null || __instance?.m_projectileAccuracy == null || __instance?.m_projectileVelMin == null || __instance?.m_projectileAccuracyMin == null)
-                    return;
-
-                float playerProjVelMinMod = Helper.applyModifierValue(__instance.m_projectileVelMin, Configuration.Current.PlayerProjectile.playerMinChargeVelocityMultiplier);
-                float playerProjVelMaxMod = Helper.applyModifierValue(__instance.m_projectileVel, Configuration.Current.PlayerProjectile.playerMaxChargeVelocityMultiplier);
-
-                // negate value to handle increasing accuracy means decreasing variance
-                float playerProjAccuMinMod = Helper.applyModifierValue(__instance.m_projectileAccuracyMin, -Configuration.Current.PlayerProjectile.playerMinChargeAccuracyMultiplier);
-                float playerProjAccuMaxMod = Helper.applyModifierValue(__instance.m_projectileAccuracy, -Configuration.Current.PlayerProjectile.playerMaxChargeAccuracyMultiplier);
-
-                if (Configuration.Current.PlayerProjectile.enableScaleWithSkillLevel)
-                {
-
-                    Skills.SkillType skillType = __instance.m_weapon.m_shared.m_skillType;
-                    if (skillType != Skills.SkillType.None) // https://github.com/valheimPlus/ValheimPlus/issues/758
-                    {
-                        Player player = (Player)__instance.m_character;
-                        Skills.Skill skill = player.m_skills.GetSkill(skillType);
-                        float maxLevelPercentage = skill.m_level * 0.01f;
-
-                        __instance.m_projectileVelMin = Mathf.Lerp(__instance.m_projectileVelMin, playerProjVelMinMod, maxLevelPercentage);
-                        __instance.m_projectileVel = Mathf.Lerp(__instance.m_projectileVel, playerProjVelMaxMod, maxLevelPercentage);
-
-                        __instance.m_projectileAccuracyMin = Mathf.Lerp(__instance.m_projectileAccuracyMin, playerProjAccuMinMod, maxLevelPercentage);
-                        __instance.m_projectileAccuracy = Mathf.Lerp(__instance.m_projectileAccuracy, playerProjAccuMaxMod, maxLevelPercentage);
-                    }
-                }
-                else
-                {
-                    __instance.m_projectileVelMin = playerProjVelMinMod;
-                    __instance.m_projectileVel = playerProjVelMaxMod;
-
-                    __instance.m_projectileAccuracyMin = playerProjAccuMinMod;
-                    __instance.m_projectileAccuracy = playerProjAccuMaxMod;
-                }
-            }
-
+            if (__instance == null) return;
+            if (Configuration.Current.PlayerProjectile.IsEnabled && __instance.IsAttackFromLocalPlayer())
+                AdjustPlayerProjectile(ref __instance);
             if (Configuration.Current.MonsterProjectile.IsEnabled && !__instance.m_character.IsPlayer())
+                AdjustEnemyProjectile(ref __instance);
+        }
+
+
+        private static void AdjustPlayerProjectile(ref Attack __instance)
+        {
+            float playerProjectileVelocityMinMod = Helper.applyModifierValue(__instance.m_projectileVelMin,
+                Configuration.Current.PlayerProjectile.playerMinChargeVelocityMultiplier);
+            float playerProjectileVelocityMaxMod = Helper.applyModifierValue(__instance.m_projectileVel,
+                Configuration.Current.PlayerProjectile.playerMaxChargeVelocityMultiplier);
+
+            // negate value to handle increasing accuracy means decreasing variance
+            float playerProjectileAccuracyMinMod = Helper.applyModifierValue(__instance.m_projectileAccuracyMin,
+                -Configuration.Current.PlayerProjectile.playerMinChargeAccuracyMultiplier);
+            float playerProjectileAccuracyMaxMod = Helper.applyModifierValue(__instance.m_projectileAccuracy,
+                -Configuration.Current.PlayerProjectile.playerMaxChargeAccuracyMultiplier);
+
+            float skillPercentage = 1f;
+            if (Configuration.Current.PlayerProjectile.enableScaleWithSkillLevel)
             {
-
-                // This might not be required but i wanted to add this non the less to be sure that no missing properties are causing issues.
-                if (__instance?.m_projectileVel == null || __instance?.m_projectileAccuracy == null || __instance?.m_projectileVelMin == null || __instance?.m_projectileAccuracyMin == null)
-                    return;
-
-                __instance.m_projectileVel = Helper.applyModifierValue(__instance.m_projectileVel, Configuration.Current.MonsterProjectile.monsterMaxChargeVelocityMultiplier);
-
-                // negate value to handle increasing accuracy means decreasing variance
-                __instance.m_projectileAccuracy = Helper.applyModifierValue(__instance.m_projectileAccuracy, -Configuration.Current.MonsterProjectile.monsterMaxChargeAccuracyMultiplier);
-
-                __instance.m_projectileVelMin = Mathf.Clamp(__instance.m_projectileVelMin, 0f, maxClampValue);
-                __instance.m_projectileVel = Mathf.Clamp(__instance.m_projectileVel, 0f, maxClampValue);
-
-                __instance.m_projectileAccuracyMin = Mathf.Clamp(__instance.m_projectileAccuracyMin, 0f, maxClampValue);
-                __instance.m_projectileAccuracy = Mathf.Clamp(__instance.m_projectileAccuracy, 0f, maxClampValue);
+                var skillType = __instance.m_weapon.m_shared.m_skillType;
+                if (skillType == Skills.SkillType.None) return; // https://github.com/valheimPlus/ValheimPlus/issues/758
+                var player = (Player)__instance.m_character;
+                skillPercentage = player.m_skills.GetSkill(skillType).m_level * 0.01f;
             }
 
-            
+            __instance.m_projectileVelMin =
+                ShortcutLerp(__instance.m_projectileVelMin, playerProjectileVelocityMinMod, skillPercentage);
+            __instance.m_projectileVel =
+                ShortcutLerp(__instance.m_projectileVel, playerProjectileVelocityMaxMod, skillPercentage);
+
+            __instance.m_projectileAccuracyMin =
+                ShortcutLerp(__instance.m_projectileAccuracyMin, playerProjectileAccuracyMinMod, skillPercentage);
+            __instance.m_projectileAccuracy =
+                ShortcutLerp(__instance.m_projectileAccuracy, playerProjectileAccuracyMaxMod, skillPercentage);
         }
+
+        private static void AdjustEnemyProjectile(ref Attack __instance)
+        {
+            __instance.m_projectileVelMin = ClampValue(__instance.m_projectileVelMin);
+            __instance.m_projectileVel = ClampValue(Helper.applyModifierValue(__instance.m_projectileVel,
+                Configuration.Current.MonsterProjectile.monsterMaxChargeVelocityMultiplier));
+
+            __instance.m_projectileAccuracyMin = ClampValue(__instance.m_projectileAccuracyMin);
+            __instance.m_projectileAccuracy = ClampValue(Helper.applyModifierValue(__instance.m_projectileAccuracy,
+                // negate value to handle increasing accuracy means decreasing variance
+                -Configuration.Current.MonsterProjectile.monsterMaxChargeAccuracyMultiplier));
+        }
+        
+        // ReSharper disable once CompareOfFloatsByEqualityOperator : expected constant 1f which is specifiable in float
+        private static float ShortcutLerp(float a, float b, float t) => t == 1f ? b : Mathf.Lerp(a, b, t);
+
+        private const float MaxClampValue = 1E+6f;
+        private static float ClampValue(float velocity) => Mathf.Clamp(velocity, 0f, MaxClampValue);
     }
 }
